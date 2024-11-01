1a79fa888772507ceea01b823db4fff86c1f5819
EnigmaCurry <ryan@enigmacurry.com>
2024-11-01 13:46:54 -0600

    interfaces

--- src/tui/interface.rs
+++ src/tui/interface.rs
@@ -7,6 +7,9 @@ use cursive::views::*;
 use cursive::Cursive;
 #[allow(unused_imports)]
 use cursive::CursiveExt;
+use strum::{AsRefStr, Display, EnumIter, EnumString, IntoEnumIterator};
+
+use super::overlay::show_overlay_dialog;
 
 fn format_interface_info(info: &InterfaceInfo) -> String {
     format!(
@@ -21,6 +24,36 @@ fn format_interface_info(info: &InterfaceInfo) -> String {
             .find(|ip| ip.contains('.'))
             .unwrap_or(&"none".to_string())
     )
+}
+
+fn rename_interface(siv: &mut Cursive, interface_name: String) {}
+
+pub fn configure_interface(siv: &mut Cursive, interface_name: String) {
+    #[derive(EnumIter, AsRefStr, EnumString, Debug, Clone, Display)]
+    enum MenuItem {
+        #[strum(serialize = "Rename Interface")]
+        RenameInterface,
+        #[strum(serialize = "Change IPv4 address")]
+        ChangeIpv4Address,
+        #[strum(serialize = "Change Gateway address")]
+        ChangeGateway,
+        #[strum(serialize = "Change DNS addresses")]
+        ChangeDNS,
+    }
+    let ifname = interface_name.clone();
+    let mut menu =
+        SelectView::<MenuItem>::new().on_submit(move |siv: &mut Cursive, choice: &MenuItem| {
+            match choice {
+                &MenuItem::RenameInterface => rename_interface(siv, ifname.clone()),
+                _ => panic!(),
+            }
+        });
+    for item in MenuItem::iter() {
+        menu.add_item(item.as_ref(), item.clone());
+    }
+    let interface_name = interface_name.to_string();
+    let dialog = Dialog::around(menu).title(format!("Rename Interface : {interface_name}"));
+    show_overlay_dialog(siv, dialog);
 }
 
 pub fn main(siv: &mut Cursive) -> LinearLayout {
@@ -49,18 +82,25 @@ pub fn main(siv: &mut Cursive) -> LinearLayout {
     }
 
     // Create the SelectView for the interface names
-    let mut menu = SelectView::<String>::new().on_select({
-        let interfaces = interfaces.clone();
-        move |siv, selected_name| {
-            let interface_info = interfaces.iter().find(|i| &i.name == selected_name);
-            if let Some(info) = interface_info {
-                let details = format_interface_info(info);
-                siv.call_on_name("info_box", |view: &mut TextView| {
-                    view.set_content(details);
-                });
+    let mut menu = SelectView::<String>::new()
+        .on_select({
+            let interfaces = interfaces.clone();
+            move |siv, selected_name| {
+                // Find the selected interface and update the info box
+                let interface_info = interfaces.iter().find(|i| &i.name == selected_name);
+                if let Some(info) = interface_info {
+                    let details = format_interface_info(info);
+                    siv.call_on_name("info_box", |view: &mut TextView| {
+                        view.set_content(details);
+                    });
+                }
             }
-        }
-    });
+        })
+        .on_submit({
+            move |siv: &mut Cursive, selected_name: &String| {
+                configure_interface(siv, selected_name.clone());
+            }
+        });
 
     for interface_info in &interfaces {
         menu.add_item(interface_info.name.clone(), interface_info.name.clone());
